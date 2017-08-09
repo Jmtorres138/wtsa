a128f66e211018b8a558edd64aecfeef0d1649da
Jason Matthew Torres <jason.matthew.torres@gmail.com>
2017-08-09 14:07:41 +0100

    update

--- promoter_driven/peakC/run_peakC.R
+++ promoter_driven/peakC/run_peakC.R
@@ -6,32 +6,43 @@ library("data.table")
 library("ggplot2")
 library("ggrepel")
 
-peakc.dir <- "/home/wellgen/jtorres/software/peakC/R/"
+#peakc.dir <- "/home/wellgen/jtorres/software/peakC/R/"
+peakc.dir <- "/Users/jtorres/Google Drive/Science/Projects/wtsa/promoter_driven/peakC/"
 source(peakc.dir %&% "util.R")
 source(peakc.dir %&% "combined_experiment_analysis.R")
 source(peakc.dir %&% "experiment_comparison.R")
 source(peakc.dir %&% "single_experiment_analysis.R")
 
-proj.dir <- "/home/wellgen/jtorres/projects/promoter-driven/"
-plot.dir <- "/home/wellgen/jtorres/projects/promoter-driven/plots/"
+serv.dir <- "/Users/jtorres/FUSE3/"
+
+proj.dir <- serv.dir %&% "promoter-driven/" #"/t1-data/user/hugheslab/jtorres/promoter-driven/"
+plot.dir <- proj.dir %&% "peakC/monotonic/plots/"
+
+probe.file <- proj.dir %&% "oligo-file.txt"
+probe.df <- fread(probe.file)
+names(probe.df) <- c("Gene","Chr","Ex.Start","Ex.End","Chrom","Start","End","V8","V9")
+
+
+rfiles.dir <- proj.dir %&% "Stat_Package_CB4/Processed_gfc/R_files/"
+rsuffix <- "_Processed_R_analysis.txt"
 
 args = commandArgs(trailingOnly=TRUE)
+#my.win <- 11; my.fdr <- 0.01
 
 if (length(args)!=2) {
   stop("Two arguments must be supplied: window and FDR", call.=FALSE)
 } else if (length(args)==2) {
   # default output file
-  my.win <- args[1] 
+  my.win <- args[1]
   my.fdr <- args[2]
 }
 
 #print(as.integer(my.win) + 1)
 
 find_chrom <- function(genename){
-	probe.df <- fread(proj.dir%&%"capture_probes.txt")
 	p.df <- filter(probe.df,Gene==genename)
 	chrom <- p.df$Chr
-	return(chrom) 
+	return(chrom)
 }
 
 split_frag <- function(s){
@@ -41,14 +52,14 @@ split_frag <- function(s){
 	vec2 <- strsplit(vec[2],split="-")[[1]]
 	start <- as.integer(vec2[1])
 	stop <- as.integer(vec2[2])
-	return(list(c,start,stop)) 
+	return(list(c,start,stop))
 }
 
 add_frag_cols <- function(df){
 	frag.vec <- df$fragment
 	chr <- c()
 	start <- c()
-	end <- c() 
+	end <- c()
 	for (i in 1:length(frag.vec)){
 		f <- frag.vec[i]
 		l <- split_frag(f)
@@ -66,8 +77,8 @@ add_frag_cols <- function(df){
 read_r_capfile <- function(genename){
 	print("Reading R Capure-C file from Telenius pipeline: " %&% genename)
 	chrom <- find_chrom(genename)
-	fname <- genename %&% "_eQTL_Capture_R_analysis.txt"
-	rdir <- "/t1-data1/WTSA_Dev/ddownes/Diabetes_Tract/eQTL_Capture/Analysis/Stat_Package/eQTL_Capture_gfc/R_files/"
+	fname <- genename %&% rsuffix #"_Processed_R_analysis.txt"
+	rdir <- rfiles.dir
 	head <- readLines(rdir%&%fname,n=1)
 	head <- strsplit(x=head,split="\t")[[1]]
 	head <- c("fragment",head)
@@ -83,44 +94,43 @@ read_r_capfile <- function(genename){
 	return(out.df)
 }
 
-make_peakc_mat <- function(cap.df, viewpoint, sampname="condB_1_norm",window=1000e3){ 
+make_peakc_mat <- function(cap.df, viewpoint, sampname="conD_1_norm",window=1000e3){
 	df <- dplyr::select(cap.df,one_of("start",sampname))
 	mat <- as.matrix(df)
 	max.count <- max(mat[,2])
 	#viewpoint <- #mat[(mat[,2]==max.count),][1]
 	print("Viewpoint: " %&% viewpoint)
-	max.pos <- viewpoint + window 
-	min.pos <- viewpoint - window 
+	max.pos <- viewpoint + window
+	min.pos <- viewpoint - window
 	sub.mat <- mat[(mat[,1]<=max.pos & mat[,1]>=min.pos),]
-	return(sub.mat) 
+	return(sub.mat)
 }
 
-make_expdata_list <- function(cap.df, view.point, window=1000e3, samp.vec){ #=c("condB_1_norm","condB_2_norm","condB_3_norm")){
+make_expdata_list <- function(cap.df, view.point, window=1000e3, samp.vec){ #=c("condD_1_norm","condD_2_norm","condD_3_norm")){
 	capclist <- list()
 	i = 0
 	for (samp in samp.vec){
-		i = i+1 
+		i = i+1
 		df <- as.data.frame(make_peakc_mat(cap.df, window=window, viewpoint=view.point,sampname=samp))
-		capclist[[i]] <- df 
+		capclist[[i]] <- df
 	}
 	return(capclist)
 }
 
 find_viewpoint <- function(genename){
-	probe.df <- fread(proj.dir%&%"capture_probes.txt")
 	p.df <- filter(probe.df,Gene==genename)
 	vp <- round(abs(mean(c(p.df$End,p.df$Start))))
-	return(vp) 
+	return(vp)
 }
 
 
 make_poly_df <- function(df){
-	poly.x <- c() 
-	poly.y <- c() 
+	poly.x <- c()
+	poly.y <- c()
 	for (i in 1:dim(df)[1]){
 		x1 <- df$start[i]
 		x2 <- df$end[i]
-		y <- df$condB_mean[i]
+		y <- df$conD_mean[i]
 		poly.x <- append(poly.x,c(x1,x1,x2,x2))
 		poly.y <- append(poly.y,c(0,y,y,0))
 	}
@@ -132,10 +142,10 @@ make_poly_df <- function(df){
 process_index_snpdf <- function(){
 	print("Processing data frame of index SNPs that guided experimental design")
 	t1 <- fread(proj.dir %&% "gwas-index.bed")
-	t2 <- fread(proj.dir %&% "eQTL-index.bed")	
+	t2 <- fread(proj.dir %&% "eQTL-index.bed")
 	snp.df <- rbind(t1,t2)
 	names(snp.df) <- c("CHR","START","END","ANNOT")
-	RSID <- c() 
+	RSID <- c()
 	for (i in 1:dim(snp.df)[1]){
 		s <- snp.df$ANNOT[i]
 		rs <- strsplit(s,"_")[[1]][1]
@@ -147,18 +157,18 @@ process_index_snpdf <- function(){
 
 
 check_sigfrags_snps <- function(df,chrom,sig.fragments){
-	# This function is mostly for plotting 
+	# This function is mostly for plotting
 	frag.start <- c()
-	frag.end <- c() 
+	frag.end <- c()
 	snp <- c()
-	snp.pos <- c() 
+	snp.pos <- c()
 	sig.df <- filter(df,start %in% sig.fragments)
 	index.df <- process_index_snpdf()
 	index.df <- select(index.df,one_of("CHR","START","RSID"))
 	index.df <- index.df[!duplicated(index.df$RSID),]
 	snp.y <- rep(0,dim(index.df)[1])
 	index.df <- cbind(index.df,snp.y)
-	index.df <- filter(index.df,CHR==chrom)
+	index.df <- filter(index.df,CHR==("chr"%&%chrom))
 	if (dim(index.df)[1] > 0){
 		for (i in 1:dim(index.df)[1]){
 			pos <- index.df$START[i]
@@ -176,17 +186,17 @@ check_sigfrags_snps <- function(df,chrom,sig.fragments){
 		}
 		if (length(snp)>0){
 			#snp.y <- rep(0,length(snp))
-			out.df <- as.data.frame(cbind(frag.start,frag.end,snp,snp.pos))	
+			out.df <- as.data.frame(cbind(frag.start,frag.end,snp,snp.pos))
 			out.df$snp.pos <- as.integer(as.character(out.df$snp.pos))
 			#out.df$snp.y <- as.integer(as.character(out.df$snp.y))
 			num <- length(unique(out.df$snp.pos))
 			print("There are " %&% num %&% " index SNPs that map to significantly interacting fragments")
 			out.df$frag.start <- as.integer(as.character(out.df$frag.start))
 			out.df$frag.end <- as.integer(as.character(out.df$frag.end))
-			return(out.df) 
+			return(out.df)
 		} else{
 			print("No index SNPS map to significant fragments")
-			return(NULL) 
+			return(NULL)
 		}
 	} else{
 		print("No index SNPs are on the chromosome")
@@ -195,21 +205,21 @@ check_sigfrags_snps <- function(df,chrom,sig.fragments){
 }
 
 get_contiguous_sigfrags <- function(overlap.frag, df, sig.fragments){
-	# This function is mostly for plotting 
-	# overlap.frag is two element vector c(start,end) 
-	vec <- c() 
+	# This function is mostly for plotting
+	# overlap.frag is two element vector c(start,end)
+	vec <- c()
 	vec <- append(vec,overlap.frag[1])
 	sig.df <- filter(df,start %in% sig.fragments) %>% arrange(start)
 	print("Checking upstream...")
 	index <- match(overlap.frag[1],df$start)
-	status <- TRUE 
+	status <- TRUE
 	count <- 0
 	while(status==TRUE){
-		index <- index - 1 
+		index <- index - 1
 		up.pos <- df$start[index]
 		if (up.pos %in% sig.df$start){
 			vec <- append(vec,up.pos)
-			count <- count + 1 
+			count <- count + 1
 			print("Upstream match count: " %&% count)
 		} else{
 			status <- FALSE
@@ -217,14 +227,14 @@ get_contiguous_sigfrags <- function(overlap.frag, df, sig.fragments){
 	}
 	print("Checking downstream...")
 	index <- match(overlap.frag[1],df$start)
-	status <- TRUE 
+	status <- TRUE
 	count <- 0
 	while(status==TRUE){
-		index <- index + 1 
+		index <- index + 1
 		down.pos <- df$start[index]
 		if (down.pos %in% sig.df$start){
 			vec <- append(vec,down.pos)
-			count <- count + 1 
+			count <- count + 1
 			print("Downstream match count: " %&% count)
 		} else{
 			status <- FALSE
@@ -232,12 +242,12 @@ get_contiguous_sigfrags <- function(overlap.frag, df, sig.fragments){
 	}
 	print("Total fragments in contiguous interaction: " %&% length(vec))
 	vec <- sort(vec)
-	return(vec) 
+	return(vec)
 }
 
 
 get_contiguous_sigfrags_df <- function(overlap, df, sig.fragments){
-	# This function is mostly for plotting 
+	# This function is mostly for plotting
 	out.df <- c()
 	for (i in 1:dim(overlap)[1]){
 		overlap.frag <- c(overlap$frag.start[i],overlap$frag.end[i])
@@ -256,11 +266,11 @@ plot_frag_over_cellmean<- function(genename,chrom,df,sig.fragments,viewpoint,win
 	print("Making polygon data frame for fragments in region")
 	full.poly.df <- make_poly_df(df)
 	saveRDS(full.poly.df, file=plot.dir %&% "dataframes_for_plots/" %&% genename %&% "_full.poly.df.RDS")
-	plt <- ggplot(data=full.poly.df, aes(x=poly.x/1e6,y=poly.y)) + 
-		geom_polygon(color="gray66",fill="gray74") + theme_bw() + 
-		coord_cartesian(xlim=c(xlow/1e6,xhigh/1e6)) + 
-		ggtitle("Locus: " %&% genename) + 
-		xlab("Position on Chromosome " %&% gsub("chr","",chrom) %&% "(Mb)") + 
+	plt <- ggplot(data=full.poly.df, aes(x=poly.x/1e6,y=poly.y)) +
+		geom_polygon(color="gray66",fill="gray74") + theme_bw() +
+		coord_cartesian(xlim=c(xlow/1e6,xhigh/1e6)) +
+		ggtitle("Locus: " %&% genename) +
+		xlab("Position on Chromosome " %&% gsub("chr","",chrom) %&% "(Mb)") +
 		ylab("Mapped Reads per Fragment")
 
 	if (length(sig.fragments)>0){
@@ -269,8 +279,8 @@ plot_frag_over_cellmean<- function(genename,chrom,df,sig.fragments,viewpoint,win
 		sig.poly.df <- make_poly_df(sig.df)
 		saveRDS(sig.poly.df, file=plot.dir %&% "dataframes_for_plots/" %&% genename %&% "_sig.poly.df.RDS")
 		plt <- plt + geom_polygon(data=sig.poly.df,aes(x=poly.x/1e6,y=poly.y),
-						color="midnightblue",fill="blue2") + 
-						geom_hline(yintercept=0,color="gray66",size=0.8) 
+						color="midnightblue",fill="blue2") +
+						geom_hline(yintercept=0,color="gray66",size=0.8)
 		overlap <- check_sigfrags_snps(df,chrom,sig.fragments)
 		if (!is.null(overlap)){
 			contig.df <- get_contiguous_sigfrags_df(overlap,df,sig.fragments)
@@ -280,8 +290,8 @@ plot_frag_over_cellmean<- function(genename,chrom,df,sig.fragments,viewpoint,win
 				saveRDS(peak.poly.df, file=plot.dir %&% "dataframes_for_plots/" %&% genename %&% "_peak.poly.df.RDS")
 
 				plt <- plt + geom_polygon(data=peak.poly.df,aes(x=poly.x/1e6,y=poly.y),
-							color="firebrick3",fill="firebrick1") + 
-							geom_hline(yintercept=0,color="gray66",size=0.85) 
+							color="firebrick3",fill="firebrick1") +
+							geom_hline(yintercept=0,color="gray66",size=0.85)
 			}
 		}
 	}
@@ -289,24 +299,25 @@ plot_frag_over_cellmean<- function(genename,chrom,df,sig.fragments,viewpoint,win
 	index.df <- process_index_snpdf()
 	index.df <- select(index.df,one_of("CHR","START","RSID"))
 	index.df <- index.df[!duplicated(index.df$RSID),]
-	index.df <- filter(index.df,CHR==chrom,START>=xlow,START<=xhigh)
+	index.df <- filter(index.df,CHR==("chr"%&%chrom),START>=xlow,START<=xhigh)
 	snp.y <- rep(0,dim(index.df)[1])
 	index.df <- cbind(index.df,snp.y)
 	saveRDS(index.df, file=plot.dir %&% "dataframes_for_plots/" %&% genename %&% "_index.df.RDS")
 
 	if (dim(index.df)[1] > 0){
 		plt <- plt + geom_point(data=index.df, aes(x=START/1e6,y=snp.y),
-			shape=23,size=2,color="black",fill="ghostwhite") + 
-			geom_label_repel(data=index.df,aes(x=START/1e6,y=snp.y,label=RSID),fill="ghostwhite") 		
+			shape=23,size=2,color="black",fill="ghostwhite") +
+			geom_label_repel(data=index.df,aes(x=START/1e6,y=snp.y,label=RSID),fill="ghostwhite")
 	}
 
 	ggsave(filename=plot.dir%&%"peakC_"%&%genename%&%".pdf",plt,width=8,height=4)
+	plt
 }
 
 
 get_contiguous_df <- function(df, sig.fragments){
 	print("Determining the number of separate contact regions...")
-	# overlap.frag is two element vector c(start,end) 
+	# overlap.frag is two element vector c(start,end)
 	fvec <- sort(sig.fragments)
 	fnum <- c()
 	count <- 0
@@ -324,7 +335,7 @@ get_contiguous_df <- function(df, sig.fragments){
 	temp.df <- as.data.frame(cbind(fvec,fnum))
 	inter.num <- c()
 	inter.start <- c()
-	inter.end <- c() 
+	inter.end <- c()
 	for (i in 1:length(unique(temp.df$fnum))){
 		sub.df <- filter(temp.df,fnum==i)
 		sub.f <- sub.df$fvec
@@ -346,13 +357,13 @@ get_contiguous_SNPoverlap_df <- function(contig.df,chrom){
 	inter.start <- c()
 	inter.end <- c()
 	snp <- c()
-	snp.pos <- c() 
+	snp.pos <- c()
 	index.df <- process_index_snpdf()
 	index.df <- select(index.df,one_of("CHR","START","RSID"))
 	index.df <- index.df[!duplicated(index.df$RSID),]
 	snp.y <- rep(0,dim(index.df)[1])
 	index.df <- cbind(index.df,snp.y)
-	index.df <- filter(index.df,CHR==chrom)
+	index.df <- filter(index.df,CHR==("chr"%&%chrom))
 	if (dim(index.df)[1] > 0){
 		for (i in 1:dim(index.df)[1]){
 			rs <- index.df$SNP[i]
@@ -365,7 +376,7 @@ get_contiguous_SNPoverlap_df <- function(contig.df,chrom){
 					inter.start <- c(inter.start, s)
 					inter.end <- c(inter.end, e)
 					snp <- c(snp, rs)
-					snp.pos <- c(snp.pos, p) 				
+					snp.pos <- c(snp.pos, p)
 				}
 			}
 		}
@@ -384,7 +395,7 @@ get_contiguous_SNPoverlap_df <- function(contig.df,chrom){
 #test <- get_contiguous_SNPoverlap_df(get_contiguous_df(df, sig.fragments))
 
 
-sig_interactions <- function(genename, n.exp=3, sample.vec=c("condB_1_norm","condB_2_norm","condB_3_norm")){
+sig_interactions <- function(genename, n.exp=3, sample.vec=c("conD_1_norm","conD_2_norm","conD_3_norm")){
 	df <- read_r_capfile(genename)
 	chrom <- find_chrom(genename)
 	print("Looking up viewpoint for experiment probe")
@@ -392,7 +403,7 @@ sig_interactions <- function(genename, n.exp=3, sample.vec=c("condB_1_norm","con
 	print("Making input list for peakC...")
 	data <- make_expdata_list(cap.df=df, view.point=vp, samp.vec=sample.vec, window=1000e3)
 	print("Running peakC to find significant Capture-C interactions...")
-	sig.fragments <- combined.analyseC(data=data, type="data", num.exp = n.exp, vp.pos = vp, 
+	sig.fragments <- combined.analyseC(data=data, type="data", num.exp = n.exp, vp.pos = vp,
 		window = (as.integer(my.win) + 1), plot = FALSE,y.max=400, alpha=my.fdr,alt=FALSE) # window = 21 # alt=T
 	#overlap <- check_sigfrags_snps(df,chrom,sig.fragments)
 	plot_frag_over_cellmean(genename,chrom,df,sig.fragments,viewpoint=vp,window=1000e3) # 500e3
@@ -403,7 +414,7 @@ sig_interactions <- function(genename, n.exp=3, sample.vec=c("condB_1_norm","con
 		for (i in 1:dim(contig.df)[1]){
 			s <- contig.df$inter.start[i]
 			e <- contig.df$inter.end[i]
-			string <- s %&% "_" %&% e 
+			string <- s %&% "_" %&% e
 			string.vec <- append(string.vec,string)
 		}
 		contacts <- paste(string.vec,collapse=",")
@@ -415,20 +426,20 @@ sig_interactions <- function(genename, n.exp=3, sample.vec=c("condB_1_norm","con
 			for (i in 1:dim(overlap.df)[1]){
 				s <- contig.df$inter.start[i]
 				e <- contig.df$inter.end[i]
-				string <- s %&% "_" %&% e 
+				string <- s %&% "_" %&% e
 				string.vec <- append(string.vec,string)
 			}
 			overlaps <- paste(string.vec,collapse=",")
 			overlap.num <- dim(overlap.df)[1]
 		} else{
-			overlaps <- NA 
-			overlap.num <- 0 
+			overlaps <- NA
+			overlap.num <- 0
 		}
 	} else{
 		contacts <- NA
-		contact.num <- 0 
-		overlaps <- NA 
-		overlap.num <- 0 
+		contact.num <- 0
+		overlaps <- NA
+		overlap.num <- 0
 	}
 	#contact.num <- dim(contig.df)[1]
 	#overlap.num <- dim(overlap.df)[1]
@@ -436,14 +447,14 @@ sig_interactions <- function(genename, n.exp=3, sample.vec=c("condB_1_norm","con
 }
 
 create_summary <- function(){
-	probe.df <- fread(proj.dir%&%"capture_probes.txt")
+	#probe.df <- fread(probe.file)
 	gene.vec <- probe.df$Gene
 	chr <- c()
 	gene <- c()
 	contacts <- c()
 	contact.regions <- c()
-	snp.overlaps <- c() 
-	overlap.regions <- c() 
+	snp.overlaps <- c()
+	overlap.regions <- c()
 	for (gg in gene.vec){
 		g.list <- sig_interactions(gg)
 		ch <- g.list[[1]]
@@ -456,8 +467,8 @@ create_summary <- function(){
 		gene <- c(gene,g)
 		contacts <- c(contacts,con)
 		contact.regions <- c(contact.regions,conint)
-		snp.overlaps <- c(snp.overlaps,ov) 
-		overlap.regions <- c(overlap.regions,ovint) 
+		snp.overlaps <- c(snp.overlaps,ov)
+		overlap.regions <- c(overlap.regions,ovint)
 	}
 	out.df <- as.data.frame(cbind(chr,gene,contacts,contact.regions,snp.overlaps,overlap.regions))
 	out.df$contacts <- as.integer(as.character(out.df$contacts))
@@ -467,12 +478,3 @@ create_summary <- function(){
 }
 
 res.df <- create_summary()
-
-
-
-
-
-
-
-
-
