9cb5cacd57566c0b92482308ebec66c84e6cc6a2
Jason Matthew Torres <jason.matthew.torres@gmail.com>
2017-08-09 14:17:15 +0100

    added

---
+++ enhancer_driven/probe_design/Finances.R
@@ -0,0 +1,34 @@
+24000 + 36000 + 6000 + 2400
+
+1200 # fragments 
+10 # pounds per olig 
+
+24000 # total cost for oligos 
+6 # libraries # 6-7 billion reads 
+# 2.5 B reads per flow cell
+# 300-350 M reads per lane 
+1500 # pounds per lane 
+36000 # rough estimate for total seq. cost 
+
+2000 # cost for QC next seq runs 
+6000 # cost for 3 QC runs 
+
+300 # libary cost 
+8 * 300 # 2400 for total library prep 
+
+# Capture-C cost 
+24000 + 36000 + 6000 + 2400 # 68400
+
+
+24000 # Hi-C estimate 
+
+# Tiled C 
+# 2-3000 pounds per site 
+# let's say 12 sites 
+3000 * 12 # 36000 cost for tiled-c 
+
+68400 + 36000 + 24000 
+68400 + 36000 + 2*24000 
+
+# 128400-152400
+
