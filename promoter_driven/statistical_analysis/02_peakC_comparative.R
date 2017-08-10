dc6cdb3856777a8dce0f992c689df7625398374e
jtorres <jtorres@deva.molbiol.ox.ac.uk>
2017-08-10 13:17:11 +0100

    arguments my.win and my.cutoff must be numeric input to compare.data function

--- promoter_driven/statistical_analysis/02_peakC_comparative.R
+++ promoter_driven/statistical_analysis/02_peakC_comparative.R
@@ -4,9 +4,9 @@ library("dplyr")
 library("data.table")
 
 
-#serv.dir <- "/Users/jtorres/FUSE2/" # Local 
-serv.dir <- "/t1-data/user/hugheslab/jtorres/" # CBRG 
-peakc.dir <- serv.dir %&% "analysis/wtsa/software/peakC/" 
+#serv.dir <- "/Users/jtorres/FUSE2/" # Local
+serv.dir <- "/t1-data/user/hugheslab/jtorres/" # CBRG
+peakc.dir <- serv.dir %&% "analysis/wtsa/software/peakC/"
 
 source(peakc.dir %&% "util.R")
 source(peakc.dir %&% "combined_experiment_analysis.R")
@@ -14,7 +14,7 @@ source(peakc.dir %&% "experiment_comparison.R")
 source(peakc.dir %&% "single_experiment_analysis.R")
 
 
-proj.dir <- serv.dir %&% "analysis/wtsa/promoter_driven/statistical_analysis/" 
+proj.dir <- serv.dir %&% "analysis/wtsa/promoter_driven/statistical_analysis/"
 data.dir <- serv.dir %&% "promoter-driven/"
 out.dir <- proj.dir %&% "output_files/"
 
@@ -23,7 +23,7 @@ probe.df <- fread(probe.file)
 names(probe.df) <- c("Gene","Chr","Ex.Start","Ex.End","Chrom","Start","End","V8","V9")
 
 
-rfiles.dir <- data.dir %&% "Stat_Package_CB4/Processed_gfc/R_files/" 
+rfiles.dir <- data.dir %&% "Stat_Package_CB4/Processed_gfc/R_files/"
 rsuffix <- "_Processed_R_analysis.txt"
 
 args = commandArgs(trailingOnly=TRUE)
@@ -39,7 +39,7 @@ if (length(args)!=2) {
 
 print(my.win); print(my.cutoff)
 
-# Functions 
+# Functions
 
 find_chrom <- function(genename){
   p.df <- filter(probe.df,Gene==genename)
@@ -97,21 +97,21 @@ read_r_capfile <- function(genename){
   return(out.df)
 }
 
-# t <- suppressWarnings(read_r_capfile("ADCY5")) # test 
+# t <- suppressWarnings(read_r_capfile("ADCY5")) # test
 
 
 make_mat_list <- function(cap.df, viewpoint,window=1000e3){
   max.pos <- viewpoint + window
   min.pos <- viewpoint - window
-  
+
   df.treat <- dplyr::select(cap.df,one_of("start","conD_mean"))
   mat.treat <- as.matrix(df.treat)
   sub.mat.treat <- mat.treat[(mat.treat[,1]<=max.pos & mat.treat[,1]>=min.pos),]
- 
+
   df.control <- dplyr::select(cap.df,one_of("start","conT_mean"))
   mat.control <- as.matrix(df.control)
-  sub.mat.control <- mat.control[(mat.control[,1]<=max.pos & mat.control[,1]>=min.pos),]  
-   
+  sub.mat.control <- mat.control[(mat.control[,1]<=max.pos & mat.control[,1]>=min.pos),]
+
   return(list(sub.mat.control,sub.mat.treat))
 }
 
@@ -123,26 +123,26 @@ find_viewpoint <- function(genename){
 }
 
 
-# Build result file 
+# Build result file
 
 build_result_file <- function(){
-  out.df <- c() 
+  out.df <- c()
   gene.vec <- probe.df$Gene
   pb <- txtProgressBar(min=0,max=length(gene.vec),style=3)
   for (i in 1:length(gene.vec)){
     setTxtProgressBar(pb,i)
     gene <- gene.vec[i]
-    df <- suppressWarnings(read_r_capfile(gene)) 
+    df <- suppressWarnings(read_r_capfile(gene))
     chrom <- find_chrom(gene)
     vp <- find_viewpoint(gene)
     data.list <- make_mat_list(df,vp)
     sig.fragments <- compare.data(x1=data.list[[1]],
                                   x2=data.list[[2]],
-                                  window=my.win,
+                                  window=as.numeric(my.win),
                                   vp.pos=vp,
-                                  abs.cut.off=my.cutoff)
+                                  abs.cut.off=as.numeric(my.cutoff))
     if (dim(sig.fragments)[1]>0){
-      stack.df <- filter(df,chr==("chr"%&%chrom),start %in% sig.fragments$pos) %>% 
+      stack.df <- filter(df,chr==("chr"%&%chrom),start %in% sig.fragments$pos) %>%
         dplyr::select(one_of("chr","start","end","conD_mean","DIF_conD_minus_conT_mean"))
       names(stack.df)[c(2,3)] <- c("frag.start","frag.end")
       capture <- rep(gene,dim(stack.df)[1])
@@ -157,7 +157,3 @@ build_result_file <- function(){
 }
 
 res.df <- build_result_file()
-
-
-
-
