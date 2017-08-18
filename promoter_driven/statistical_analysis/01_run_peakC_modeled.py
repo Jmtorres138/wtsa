ac79a35143cd0a9bfefd4db1e569c5813f404793
jtorres <jtorres@deva.molbiol.ox.ac.uk>
2017-08-18 10:17:08 +0100

    updated 01 scripts

--- promoter_driven/statistical_analysis/01_run_peakC_modeled.py
+++ promoter_driven/statistical_analysis/01_run_peakC_modeled.py
@@ -1,25 +1,54 @@
-#! /user/bin/python -O 
+#! /user/bin/python -O
 # Jason Matthew Torres
 '''
-Python script for submitting multiple peakC runs 
-Usage: python 01_run_peakC_modeled.py 
+Python script for submitting multiple peakC runs
+Usage: python 01_run_peakC_modeled.py
 '''
-
-import subprocess as sp 
+import os,sys
+import subprocess as sp
 
 stat_dir = "/t1-data/user/hugheslab/jtorres/analysis/wtsa/promoter_driven/statistical_analysis/"
+out_dir = stat_dir +"output_files/"
+ref_dir = "/t1-data/user/hugheslab/jtorres/analysis/wtsa/promoter_driven/reference_files/"
 
+gwas_bed = ref_dir + "genetic_credible-sets_independent_ppa01.bed"
+write_file = out_dir + "enrich_peakC-modeled_gwasPPA-01.txt"
 
 def run_job(win, fdr):
+	output_file = out_dir+"peakC-modeled_fdr"+str(fdr)+"_win"+str(win)+".txt"
 	command = ["Rscript", "--vanilla", stat_dir+"01_peakC_modeled.R", str(win), str(fdr)]
 	com = " ".join(command)
-	print com
-	sp.check_call(com,shell=True)
+	command2 = ["python",stat_dir+"01_optimize_peakC-modeled.py",output_file,gwas_bed,write_file]
+	com2 = " ".join(command2)
+
+	job_name = str(win)+"_"+str(fdr)
+	fout = open(stat_dir+"jobs/"+"job_optim_mod-"+job_name+".sh",'w')
+	script = '''
+#!/bin/bash -l
+#$ -cwd
+#$ -M jtorres
+#$ -m eas
+#$ -j n
+#$ -N optim_mod_%s
+
+module add bedtools
+
+%s
+%s
+
+	''' % (job_name,com,com2)
+	fout.write(script)
+	fout.close()
+	command = ["qsub", stat_dir+"jobs/"+"job_optim_mod-"+job_name+".sh"]
+	if os.path.exists(output_file)==False:
+		sp.check_call(" ".join(command),shell=True)
+		#pass
+
 
 def main():
-	fdr_list = ["0.01"]#,"0.20"]#,"0.05","0.10","0.20"]
+	fdr_list = ["0.01","0.05"]#,"0.10","0.20"]
 	for f in fdr_list:
-		for w in range(11,12):
+		for w in range(1,100):
 			run_job(w,f)
 
-if (__name__=="__main__"): main() 
+if (__name__=="__main__"): main()
