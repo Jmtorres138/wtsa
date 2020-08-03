c456167c4bcff348c6ab0effef84113ace3088f0
Jason <jason@jason-XPS-13-9370gmail.com>
2020-08-03 12:40:55 +0100

    added another conditional statement to prevent crashing

--- joint_analyses/5.0_caqtl-coloc-by-chrom.R
+++ joint_analyses/5.0_caqtl-coloc-by-chrom.R
@@ -41,52 +41,58 @@ gwas_caqtl_coloc <- function(gwas.signal.dir,gwas.signal,
   region.gr <- GRanges(seqnames = unique(sig.df$CHR),
                        IRanges(start=(pos-500000),end=(pos+500000)))
   caqtl.sub.df <- caqtl.df[(caqtl.gr %over% region.gr),]
-  peak.vec <- caqtl.sub.df$feature_id %>% unique(.)
-  pb <- txtProgressBar(min=1,max=length(peak.vec),style=3)
-  for (i in 1:length(peak.vec)){
-    #print(i)
-    setTxtProgressBar(pb,i)
-    peak <- peak.vec[i]
-    peak.sub.df <- filter(caqtl.sub.df,feature_id==peak)
-    sig.sub.df <- sig.df[(sig.df$POS %in% peak.sub.df$snp_pos),]
-    overlap.pos <- sig.sub.df$POS
-    overlap.snps <- sig.sub.df$SNPID
-    names(overlap.pos) <- overlap.snps
-    p.vec.gwas <- c()
-    p.vec.caqtl <- c() 
-    maf.vec <- c()
-    for (posit in overlap.pos){
-      p1 <- filter(sig.df,POS==posit)$PVAL
-      names(p1) <- filter(sig.df,POS==posit)$SNPID
-      p2 <- filter(peak.sub.df,snp_pos==posit)$p_chi2
-      names(p2) <- filter(sig.df,POS==posit)$SNPID
-      f <- filter(sig.df,POS==posit)$'F'
-      maf <- min(f,(1-f))
-      if (length(p1)==length(p2) & length(p2)==length(maf)){
-        p.vec.gwas <- append(p.vec.gwas,p1)
-        p.vec.caqtl <- append(p.vec.caqtl,p2)
-        maf.vec <- append(maf.vec,maf)       
+  if (dim(caqtl.sub.df)[1]>1){
+    peak.vec <- caqtl.sub.df$feature_id %>% unique(.)
+    pb <- txtProgressBar(min=1,max=length(peak.vec),style=3)
+    for (i in 1:length(peak.vec)){
+      #print(i)
+      setTxtProgressBar(pb,i)
+      peak <- peak.vec[i]
+      peak.sub.df <- filter(caqtl.sub.df,feature_id==peak)
+      sig.sub.df <- sig.df[(sig.df$POS %in% peak.sub.df$snp_pos),]
+      overlap.pos <- sig.sub.df$POS
+      overlap.snps <- sig.sub.df$SNPID
+      names(overlap.pos) <- overlap.snps
+      p.vec.gwas <- c()
+      p.vec.caqtl <- c() 
+      maf.vec <- c()
+      for (posit in overlap.pos){
+        p1 <- filter(sig.df,POS==posit)$PVAL
+        names(p1) <- filter(sig.df,POS==posit)$SNPID
+        p2 <- filter(peak.sub.df,snp_pos==posit)$p_chi2
+        names(p2) <- filter(sig.df,POS==posit)$SNPID
+        f <- filter(sig.df,POS==posit)$'F'
+        maf <- min(f,(1-f))
+        if (length(p1)==length(p2) & length(p2)==length(maf)){
+          p.vec.gwas <- append(p.vec.gwas,p1)
+          p.vec.caqtl <- append(p.vec.caqtl,p2)
+          maf.vec <- append(maf.vec,maf)       
+        } else{
+          print(posit)
+        }
+      }
+      exclude.vec <- is.na(maf.vec) | is.na(p.vec.gwas) | is.na(p.vec.caqtl) 
+      p.vec.gwas <- p.vec.gwas[!exclude.vec]
+      p.vec.caqtl <- p.vec.caqtl[!exclude.vec]
+      maf.vec <- maf.vec[!exclude.vec]
+      if (length(maf.vec)>0 & length(maf.vec)==length(p.vec.gwas) & 
+          length(maf.vec==p.vec.caqtl)){
+          coloc.res <- suppressMessages(coloc.abf(dataset1=list(pvalues=p.vec.gwas,
+                                               snp=names(p.vec.gwas),
+                                             N=n.gwas,s=case.prop,type="cc"),
+                               dataset2=list(pvalues=p.vec.caqtl,N=n.caqtl,
+                                             snp=names(p.vec.caqtl),type="quant"),
+                               MAF=maf.vec))
+          out.name <- out.dir %&% gwas.signal %&% ":" %&% peak %&% ".coloc.RDS"
+          saveRDS(object=coloc.res,file=out.name)      
       } else{
-        print(posit)
+        print("Not enough information present for Peak to run COLOC: " %&% peak)
       }
     }
-    exclude.vec <- is.na(maf.vec) | is.na(p.vec.gwas) | is.na(p.vec.caqtl) 
-    p.vec.gwas <- p.vec.gwas[!exclude.vec]
-    p.vec.caqtl <- p.vec.caqtl[!exclude.vec]
-    maf.vec <- maf.vec[!exclude.vec]
-    if (length(maf.vec)>0 & length(maf.vec)==length(p.vec.gwas) & 
-        length(maf.vec==p.vec.caqtl)){
-        coloc.res <- suppressMessages(coloc.abf(dataset1=list(pvalues=p.vec.gwas,
-                                             snp=names(p.vec.gwas),
-                                           N=n.gwas,s=case.prop,type="cc"),
-                             dataset2=list(pvalues=p.vec.caqtl,N=n.caqtl,
-                                           snp=names(p.vec.caqtl),type="quant"),
-                             MAF=maf.vec))
-        out.name <- out.dir %&% gwas.signal %&% ":" %&% peak %&% ".coloc.RDS"
-        saveRDS(object=coloc.res,file=out.name)      
-    } else{
-      print("Not enough information present for Peak: " %&% peak)
-    }
+  } else{
+    print("Not sufficient caQTL overlap for signal: " %&% gwas.signal)
+    out.name <- out.dir %&% gwas.signal %&% ":" %&% "FAILED" %&% ".coloc.RDS"
+    saveRDS(object=gwas.signal,file=out.name)  
   }
 }
 
