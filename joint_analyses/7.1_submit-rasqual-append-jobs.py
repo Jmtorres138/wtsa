dd2bd6c570ba68b49a46cbe7a9f90049c4c73cf1
Jason Torres <jmtorres138@gmail.com>
2020-09-28 10:52:38 +0100

    updated and currently running 7.0 and 7.1 scripts

--- joint_analyses/7.1_submit-rasqual-append-jobs.py
+++ joint_analyses/7.1_submit-rasqual-append-jobs.py
@@ -2,7 +2,7 @@
 # Jason Matthew Torres
 '''
 module load Python/3.7.4-GCCcore-8.3.0
-python 02.4_plink-glm-gwas.py
+python 07.1_submit-rasqual-append-jobs.py
 '''
 # libraries
 import sys,os
@@ -10,18 +10,17 @@ import subprocess as sp
 
 work_dir = "/well/mccarthy/users/jason/projects/wtsa/joint_analyses/"
 coloc_dir = work_dir + "coloc_analysis_files/"
-gwas_sig_dir = coloc_dir + "conditioned_gwas_summarystats/"
+rasqual_dir = coloc_dir + "rasqual_output/"
 job_dir = coloc_dir + "jobs/caqtl/"
 log_dir = coloc_dir +"logs/caqtl/"
 
-
-def submit_coloc_job(chromo):
+def submit_append_job(chromo):
     script = '''#!/bin/bash
 #$ -P mccarthy.prjc
-#$ -N coloc-job-%s
-#$ -q himem.qh
-#$ -o %s.out
-#$ -e %s.err
+#$ -N append-job-%s
+#$ -q long.qc
+#$ -o %s_append.out
+#$ -e %s_append.err
 
 echo "------------------------------------------------"
 echo "Run on host: "`hostname`
@@ -32,22 +31,21 @@ echo "------------------------------------------------"
 
 module load R/3.6.2-foss-2019b
 Rscript %s %s
-    ''' % (chromo,log_dir+chromo,log_dir+chromo,
-    work_dir+"5.0_caqtl-coloc-by-chrom.R",chromo)
-    fout = open(job_dir + chromo + "_job.sh",'w')
+    ''' % (chromo,log_dir+"chr"+chromo,log_dir+"chr"+chromo,
+    work_dir+"7.0_rasqual-caqtls_add-header-pvals.R",chromo)
+    fout = open(job_dir + "chr" + chromo + "_append.job.sh",'w')
     fout.write(script)
     fout.close()
-    #command = ["qsub",job_dir+chromo+"_job.sh"]
-    command = ["sh",job_dir+chromo+"_job.sh"] # run serially
+    command = ["qsub",job_dir+"chr" + chromo+"_append.job.sh"]
+    #command = ["sh",job_dir+"chr" + chromo+"_append.job.sh"] # run serially
     sp.check_call(command)
 
 def main():
-    file_list = os.listdir(gwas_sig_dir)
-    #chrom_list = list(set([x.split("-")[-2] for x in file_list]))
-    chrom_list = ["chr2","chr3","chr4","chr5","chr6","chr7","chr8",
-                  "chr9","chr10"]
+    chrom_list = ["2","3","4","5","7","8",
+                  "9","10","11","14","15","16","17"]
+    #chrom_list = ["18"]
     for chrom in chrom_list:
-        submit_coloc_job(chrom)
+        submit_append_job(chrom)
 
 if (__name__=="__main__"):
      main()
