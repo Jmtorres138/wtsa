d32a212851c7d9f8bb026dc2fb08f364b9e0bd65
Jason Torres <jmtorres138@gmail.com>
2021-01-18 17:19:07 +0000

    added and updated 00.2 script

--- enhancer_driven/second_round/rerun_pipeline/00.2_BLAT.sh
+++ enhancer_driven/second_round/rerun_pipeline/00.2_BLAT.sh
@@ -1,12 +1,10 @@
 
 
-
 pipePath="/home/molhaem2/telenius/CCseqBasic/CS5"
 Sample="Endo_A"
 Genome="hg19"
-OligoFile="/t1-data/user/hugheslab/jtorres/wtsa/enhancer_driven/second_round/OligoFile.txt"
-PublicPath="/public/jtorres/wtsa/enhancer_driven/second_round/${Sample}"
-
+OligoFile="/t1-data/user/hugheslab/jtorres/wtsa/enhancer_driven/second_round/rerun_pipeline/OligoFileRedo.txt"
+PublicPath="/public/jtorres/wtsa/enhancer_driven/second_round/rerun_pipeline/${Sample}"
 
 
 ${pipePath}/pipe.sh -o "${OligoFile}" -s "${Sample}" --pf "${PublicPath}" --genome ${Genome} --R1 /t1-data/user/hugheslab/jtorres/wtsa/enhancer_driven/second_round/fastq_files/original_sequencing/Endo_A_R1.fastq --R2 /t1-data/user/hugheslab/jtorres/wtsa/enhancer_driven/second_round/fastq_files/original_sequencing/Endo_A_R2.fastq --onlyBlat
