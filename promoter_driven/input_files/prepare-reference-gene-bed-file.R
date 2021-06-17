60ec69fc47d30b896b966d79e314e49d532938b1
Jason Torres <jasont@NDPH7917.local>
2021-06-17 11:24:34 +0100

    completed

--- promoter_driven/input_files/prepare-reference-gene-bed-file.R
+++ promoter_driven/input_files/prepare-reference-gene-bed-file.R
@@ -1,1 +1,30 @@
+"%&%" <- function(a,b) paste0(a,b)
 library(rtracklayer)
+library("data.table")
+library("tidyverse")
+rescomp.dir <- "/Users/jasont/science/servers/FUSE5/"
+work.dir <- rescomp.dir %&% 
+  "projects/wtsa/promoter_driven/"
+input.dir <- work.dir %&% "input_files/"
+gtf.dir <- rescomp.dir %&% "datasets/GTEx/TOPMED_reference_files/"
+
+gtf.file <- gtf.dir %&% "gencode.v30.GRCh38.ERCC.genes.collapsed_only.gtf.gz"
+gtf <- rtracklayer::import(gtf.file)
+gtf.df <- as.data.frame(gtf)
+
+gtf.file2 <- gtf.dir %&% "gencode.v30.GRCh38.annotation.ERCC.gtf.gz"
+gtf2 <- rtracklayer::import(gtf.file2)
+gtf.df2 <- as.data.frame(gtf2)
+
+gtf.df %>% filter(type=="transcript") %>% dim(.)
+gtf.df2 %>% filter(type=="transcript") %>% dim(.)
+trans.df <- gtf.df2 %>% filter(type=="transcript")
+
+# Will write a bed file with all TRANSCRIPTs to use a reference in plots 
+bed.df <- data.frame(V1=trans.df$seqnames,V2=trans.df$start,V3=trans.df$end,
+                     V4=trans.df$gene_name,stringsAsFactors = F)
+write.table(x=bed.df,file=input.dir%&%
+      "gencode.v30.GRCh38.annotation.ERCC.transcripts.bed",
+      sep="\t",quote=F,row.names=F,col.names=F)
+
+
