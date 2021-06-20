0938d7e37d47e6aff4dbc11c85db9114e48b5810
Jason Torres <jasont@NDPH7917.local>
2021-06-20 18:57:38 +0100

    added scripts

---
+++ promoter_driven/04.0_peaky-map-interactions.R
@@ -0,0 +1,108 @@
+"%&%" <- function(a,b) paste0(a,b)
+#library("data.table")
+#library("tidyverse")
+library(peaky)
+options(bitmapType='cairo')
+work.dir <- "/well/mccarthy/users/jason/projects/wtsa/joint_analyses/01_interaction-mapping/"
+output.dir <- work.dir %&% "peaky_interactions/"
+plot.dir <- output.dir %&% "plots/"
+args <- commandArgs(trailingOnly=TRUE)
+experiment.name <- args[1] #"promoter-capture"
+bait.id <- args[2] #471258
+BTS <- readRDS(file=output.dir %&% experiment.name %&% ".BTS.RDS")
+
+relevant_bait = BTS[baitID==bait.id]
+zoom = relevant_bait[abs(relevant_bait$dist)<1e6]
+
+png(filename = plot.dir %&% "adj-readcount/" %&% experiment.name %&%
+      "." %&% bait.id %&% ".adj-readcount.png")
+  plot(x=zoom$dist,
+     y=zoom$residual,
+     xlab="Distance from bait (bp)",
+     ylab="Adjusted readcount",
+     main=paste("Bait",unique(zoom$baitID)))
+dev.off()
+
+#Isolate the highest peak and adjusted readcounts within 40 kbp
+peak_center = zoom$dist[which.max(zoom$residual)]
+zoom$distance_from_peak = abs(zoom$dist-peak_center)
+single_peak = zoom[distance_from_peak<4e5,]
+
+#Define the distance function and find the best fit for omega
+dist_exp = function(offset, strength, omega_power){
+  omega = 10^omega_power
+  strength * exp(-(abs(offset * omega)))
+}
+
+## Need to try to fit omega for capture, otherwise will default to -3.8 for omega_power
+## see Downes et al. 2019
+
+try_fit_omega <- function(single_peak) {
+    omega_power_value <- tryCatch(
+        {
+            message("Fitting omega for capture...")
+            fit_omega = nls(residual~dist_exp(distance_from_peak,strength,
+              omega_power),data=single_peak,
+              start=list(strength=5, omega_power=-3))
+            png(filename = plot.dir %&% "peak-decay/" %&% experiment.name %&%
+                  "." %&% bait.id %&% ".peak-decay.png")
+              plot(single_peak$distance_from_peak, single_peak$residual,
+                    main=paste0("Decay of signal for an isolated peak\nEstimate of omega = 10^",
+                               round(coefficients(fit_omega)["omega_power"],3)),
+                    xlab="Distance from center of peak (bp)",
+                    ylab="Adjusted readcount")
+              lines(single_peak$distance_from_peak, fitted.values(fit_omega), col="red", lwd=3)
+            dev.off()
+            round(coefficients(fit_omega)["omega_power"],3) #omega_power value that will be returned
+        },
+        error=function(cond) {
+            message("Failed to fit omega, will set to -3.8:")
+            message("Here's the original error message:")
+            message(cond)
+            # Return value -3.8 in case of error
+            return(-3.8)
+        },
+        warning=function(cond) {
+            message("Omega fit caused a warning")
+            message("Here's the original warning message:")
+            message(cond)
+            # Return value -3.8 in case of warning
+            return(-3.8)
+        },
+        finally={
+            message("Completed attempt to fit omega value ")
+        }
+    )
+    return(omega_power_value)
+}
+
+##omega_power <- -3.8 #try_fit_omega(single_peak) # Manually setting to -3.8 # initially set omega to a fixed value
+omega_power <- try_fit_omega(single_peak)  # defaults to -3.8 if fitting procedure fails
+
+
+relevant_bait = BTS[baitID==bait.id]
+
+PKS = peaky(relevant_bait, omega_power, iterations=10e6) # crank up to 10 million
+
+P = interpret_peaky(relevant_bait, PKS, omega_power)
+P$omega.power <- omega_power
+#write.table(x=P,file=output.dir %&% experiment.name %&% "/" %&%
+#            experiment.name %&% "." %&% bait.id %&%
+#            ".peaky-output.txt",sep="\t",quote=F,row.names=F,col.names=T)
+write.table(x=P,file=output.dir %&% experiment.name %&% "/vary_omega/" %&%
+            experiment.name %&% "." %&% bait.id %&%
+            ".peaky-output.txt",sep="\t",quote=F,row.names=F,col.names=T) # this time allowing omega value to vary
+
+png(filename = plot.dir %&% "mppc-plots/vary_omega/" %&% experiment.name %&%
+      "." %&% bait.id %&% ".mppc.png")
+  par(mfrow=c(3,1))
+  zoom = P[abs(P$dist)<1e6]
+  plot(x=zoom$dist, xlab="Distance from bait (bp)",
+      y=zoom$residual, ylab="Adjusted readcount")
+  plot(x=zoom$dist, xlab="Distance from bait (bp)",
+      y=zoom$beta_mean, ylab="Mean contact strength",
+      col="green")
+  plot(x=zoom$dist, xlab="Distance from bait (bp)",
+      y=zoom$rjmcmc_pos, ylab="MPPC",
+      col="blue")
+dev.off()
---
+++ promoter_driven/04.2_peaky-submit-jobs.py
@@ -0,0 +1,94 @@
+#! /user/bin/python -O
+# Jason Matthew Torres
+'''
+Usage:
+module load Python/3.7.4-GCCcore-8.3.0
+python 1.1.2.1
+'''
+
+import os,sys,gzip
+import subprocess as sp
+serv_dir = "/well/mccarthy/users/jason/"
+work_dir = serv_dir+"projects/wtsa/joint_analyses/01_interaction-mapping/"
+output_dir = work_dir + "peaky_interactions/"
+log_dir = output_dir + "logs/promoter-capture/vary_omega/" # trying w/ varying omega values for promoter capture baits
+job_dir = output_dir + "jobs/promoter-capture/vary_omega/" # trying w/ varying omega values for promoter capture baits
+
+def read_bait_list(bts_file):
+    fin = open(bts_file,'r')
+    header = fin.readline()
+    out_list = []
+    for line in fin:
+        l = line.strip().split()
+        bait_id = l[0]
+        out_list.append(bait_id)
+    fin.close()
+    out_list = list(set(out_list))
+    return(out_list)
+
+def submit_job(exeriment,exp_code,bait_id):
+    script = '''#!/bin/bash
+#$ -P mccarthy.prjc
+#$ -N peaky-%s-%s
+#$ -q short.qc
+#$ -pe shmem 1
+#$ -o %s.out
+#$ -e %s.err
+
+echo "------------------------------------------------"
+echo "Run on host: "`hostname`
+echo "Operating system: "`uname -s`
+echo "Username: "`whoami`
+echo "Started at: "`date`
+echo "------------------------------------------------"
+
+module load R/3.6.2-foss-2019b
+Rscript %s %s %s
+    ''' % (exp_code,bait_id,
+    log_dir+exeriment+"."+bait_id,
+    log_dir+exeriment+"."+bait_id,
+    work_dir+"1.1.0_peaky-map-interactions.R",exeriment,bait_id)
+    fout = open(job_dir+exeriment+"."+bait_id + ".job.sh",'w')
+    fout.write(script)
+    fout.close()
+    command = ["qsub",job_dir+exeriment+"."+bait_id + ".job.sh"]
+    sp.check_call(command)
+
+def prom_jobs():
+    exper_name = "promoter-capture"
+    exper_code = "p"
+    bait_list = read_bait_list(output_dir + exper_name + ".BTS.txt")
+    for bait in bait_list:
+        #outfile = output_dir + exper_name +"/"+ exper_name +"."+bait+".peaky-output.txt" # first chain
+        outfile = output_dir + exper_name +"/vary_omega/"+ exper_name +"."+bait+".peaky-output.txt" # this time allowing omega value to vary
+        if os.path.isfile(outfile)==False:
+            #print(bait)
+            submit_job(exper_name,exper_code,bait)
+
+def e1_jobs():
+    exper_name = "enhancer1st-capture"
+    exper_code = "e1"
+    bait_list = read_bait_list(output_dir + exper_name + ".BTS.txt")
+    for bait in bait_list:
+        outfile = output_dir + exper_name +"/"+ exper_name +"."+bait+".peaky-output.txt"
+        if os.path.isfile(outfile)==False:
+            #print(bait)
+            submit_job(exper_name,exper_code,bait)
+
+def e2_jobs():
+    exper_name = "enhancer2nd-capture"
+    exper_code = "e2"
+    bait_list = read_bait_list(output_dir + exper_name + ".BTS.txt")
+    for bait in bait_list:
+        outfile = output_dir + exper_name +"/"+ exper_name +"."+bait+".peaky-output.txt"
+        if os.path.isfile(outfile)==False:
+            print(bait)
+            submit_job(exper_name,exper_code,bait)
+
+def main():
+    prom_jobs()
+    #e1_jobs()
+    #e2_jobs()
+
+if (__name__=="__main__"):
+    main()
