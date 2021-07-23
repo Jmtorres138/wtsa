3e5af1675de67e596c8814d078980a08e53319b1
Jason Torres <jasont@NDPH7917.local>
2021-07-23 18:18:14 +0100

    added

---
+++ enhancer_driven/first_round/03_CaptureCompare/03.1_create-cc-input-file.R
@@ -0,0 +1,15 @@
+"%&%" <- function(a,b) paste0(a,b)
+library("dplyr");library("data.table")
+work.dir <- "/well/mccarthy/users/jason/projects/wtsa/enhancer_driven/first_round/"
+oligo.file <- work.dir %&% "input_files/Oligo-file_merged_GRCh38.txt"
+out.dir <- work.dir %&% "03_CaptureCompare/"
+oligo.df <- fread(oligo.file,header=F)
+
+out.df <- data.frame("V1"=oligo.df$V1,"V2"=oligo.df$V2,"V3"=oligo.df$V3,
+            "V4"=oligo.df$V4,"V5"=oligo.df$V6,"V6"=oligo.df$V7,
+            "V7"=oligo.df$V3-200000,"V8"=oligo.df$V4+200000,"V9"=250,"V10"=5000,
+          stringsAsFactors=FALSE)
+out.df$V7 <- as.integer(out.df$V7)
+out.df$V8 <- as.integer(out.df$V8)
+write.table(x=out.df,file=out.dir%&%"capture-compare-input_hg38.txt",
+  quote=F,sep="\t",row.names=F,col.names=F)
