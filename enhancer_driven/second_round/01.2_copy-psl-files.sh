21df962d858b67da790a2c6c83553e5194198177
Jason Torres <jasont@NDPH7917.local>
2021-07-23 16:22:26 +0100

    nc

---
+++ enhancer_driven/second_round/01.2_copy-psl-files.sh
@@ -0,0 +1,3 @@
+WORKDIR=/well/mccarthy/users/jason/projects/wtsa/enhancer_driven/second_round
+OUTDIR=$WORKDIR/01_REUSE_blat
+cp $WORKDIR/blat/output_files/*/BlatPloidyFilterRun/REUSE_blat/*.psl $OUTDIR
