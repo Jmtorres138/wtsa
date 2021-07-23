0d06856dacebd8d9441c39e204d80054a0078692
Jason Torres <jasont@NDPH7917.local>
2021-07-23 12:52:07 +0100

    nc

---
+++ enhancer_driven/second_round/00.2.2_remove-chr-string.R
@@ -0,0 +1,12 @@
+"%&%" <- function(a,b) paste0(a,b)
+library("data.table");library("dplyr");library("purrr")
+work.dir <- "/well/mccarthy/users/jason/projects/wtsa/enhancer_driven/second_round/"
+oligo.df <- fread(work.dir %&% "input_files/Oligo-file_merged_GRCh38.txt",
+                  header=F)
+oligo.df$V2 <- purrr::map(oligo.df$V2,function(s){gsub("chr","",s)}) %>%
+as.character(.)
+oligo.df$V5 <- purrr::map(oligo.df$V5,function(s){gsub("chr","",s)}) %>%
+as.character(.)
+write.table(x=oligo.df,
+  file=work.dir%&%"input_files/Oligo-file_merged_GRCh38_no-chr.txt",
+  quote=F,sep="\t",row.names=F,col.names=F)
