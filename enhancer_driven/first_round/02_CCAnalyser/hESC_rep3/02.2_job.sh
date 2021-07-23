7039f3e68e0212157c4da24a9103c4cb9f925825
Jason Torres <jmtorres138@gmail.com>
2021-07-23 17:24:22 +0100

    added 02 scripts for first round

---
+++ enhancer_driven/first_round/02_CCAnalyser/hESC_rep3/02.2_job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#$ -cwd
+#$ -P mccarthy.prjc
+#$ -N E1_hescRep3_ccanalyser
+#$ -q short.qc
+#$ -o /well/mccarthy/users/jason/projects/wtsa/enhancer_driven/first_round/logs/hescRep3_ccanalyser.out
+#$ -e /well/mccarthy/users/jason/projects/wtsa/enhancer_driven/first_round/logs/hescRep3_ccanalyser.err
+#$ -pe shmem 16
+
+echo "------------------------------------------------"
+echo "Run on host: "`hostname`
+echo "Operating system: "`uname -s`
+echo "Username: "`whoami`
+echo "Started at: "`date`
+echo "------------------------------------------------"
+
+sh /well/mccarthy/users/jason/projects/wtsa/enhancer_driven/first_round/02_CCAnalyser/hESC_rep3/01.1_capC-CS5_hescRep3.sh
