78d0291ac7f5cc416a40368995afdb882c8204f4
Jason Torres <jmtorres138@gmail.com>
2021-09-12 21:14:41 +0100

    corrected file path

--- enhancer_driven/first_round/03_CaptureCompare/03.1_create-cc-input-file.R
+++ enhancer_driven/first_round/03_CaptureCompare/03.1_create-cc-input-file.R
@@ -1,11 +1,11 @@
 "%&%" <- function(a,b) paste0(a,b)
 library("dplyr");library("data.table")
 work.dir <- "/well/mccarthy/users/jason/projects/wtsa/enhancer_driven/first_round/"
-oligo.file <- work.dir %&% "input_files/Oligo-file_merged_GRCh38.txt"
+oligo.file <- work.dir %&% "input_files/Oligo-file_merged_GRCh38_no-chr.txt"
 out.dir <- work.dir %&% "03_CaptureCompare/"
 oligo.df <- fread(oligo.file,header=F)
 
-out.df <- data.frame("V1"=oligo.df$V1,"V2"=oligo.df$V2,"V3"=oligo.df$V3,
+out.df <- data.frame("V1"=oligo.df$V1,"V2"="chr"%&%oligo.df$V2,"V3"=oligo.df$V3,
             "V4"=oligo.df$V4,"V5"=oligo.df$V6,"V6"=oligo.df$V7,
             "V7"=oligo.df$V3-200000,"V8"=oligo.df$V4+200000,"V9"=250,"V10"=5000,
           stringsAsFactors=FALSE)
