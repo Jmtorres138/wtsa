dc1d4b7327249b33aa9d1f910c318afd550189bd
Jason Torres <jmtorres138@gmail.com>
2022-03-22 22:55:09 +0000

    updated messaging

--- promoter_driven/07.0_peak-feature-enrichments.R
+++ promoter_driven/07.0_peak-feature-enrichments.R
@@ -10,13 +10,13 @@ feature.name <- args[2]
 feature.df <- fread(feature.bed.file,header=F)
 regions.df <- fread(work.dir%&%"output_files/genomic-regions.txt",
                     header=T)
-## EndoC-betaH1 NG Capture-C peaky interactions (merged) 
+## EndoC-betaH1 NG Capture-C peaky interactions (merged)
 capc.pky.df <- fread(work.dir%&%"peaky_interactions/"%&%
   "promoter_endo_peaky-interactions.txt")
 capc.pky.df$capture <- purrr::map(capc.pky.df$capture,function(s){
   gsub("C15orf38-AP3S2","ARPIN-AP3S2",s)
 }) %>% as.character(.)
-
+peaky.df <- capc.pky.df
 ## Islet pcHi-C peaky interactions (merged)
 bed.dir<-work.dir%&%"pcHiC-peaky/"
 bed.files <- list.files(bed.dir)
@@ -24,15 +24,34 @@ pchic.pky.df <- c()
 for (f in bed.files){
   f.df <- fread(bed.dir%&%f)
   s <- strsplit(f,split="_")[[1]][5]
-  print(s)
+  ##print(s)
   f.df$V4<-s
   names(f.df) <- c("seqnames","start","end","capture")
   pchic.pky.df <- rbind(pchic.pky.df,f.df)
 }
 
-## Development with ADCY5 
-capt <- "ADCY5"
-peaky.df <- capc.pky.df 
+message <- function (..., domain = NULL, appendLF = TRUE)
+{
+    args <- list(...)
+    cond <- if (length(args) == 1L && inherits(args[[1L]], "condition")) {
+        if (nargs() > 1L)
+            warning("additional arguments ignored in message()")
+        args[[1L]]
+    }
+    else {
+        msg <- .makeMessage(..., domain = domain, appendLF = appendLF)
+        call <- sys.call()
+        simpleMessage(msg, call)
+    }
+    defaultHandler <- function(c) {
+        cat(conditionMessage(c), file = stdout(), sep = "")
+    }
+    withRestarts({
+        signalCondition(cond)
+        defaultHandler(cond)
+    }, muffleMessage = function() NULL)
+    invisible()
+}
 
 get_gr_obs <- function(capt,feature.df,peaky.df){
   cap.df <- filter(regions.df,gene==capt)
@@ -51,20 +70,19 @@ get_gr_obs <- function(capt,feature.df,peaky.df){
 }
 window_cont_table <- function(windows.gr,feature.gr,pky.gr){
   win.gr <- windows.gr[[1]][(width(windows.gr[[1]])>1)]
-  cell.a <- 0 # both pky peak and feature 
-  cell.b <- 0 # No pky peak and feature 
-  cell.c <- 0 # pky peak but No feature 
-  cell.d <- 0 # No pky peak and No feature 
-  pb <- txtProgressBar(min=1,max=length(win.gr),style=3)
+  cell.a <- 0 # both pky peak and feature
+  cell.b <- 0 # No pky peak and feature
+  cell.c <- 0 # pky peak but No feature
+  cell.d <- 0 # No pky peak and No feature
+  ##pb <- txtProgressBar(min=1,max=length(win.gr),style=3)
   for (i in 1:length(win.gr)){
-    setTxtProgressBar(pb,i)
+    ##setTxtProgressBar(pb,i)
     in.peak <- win.gr[i] %over% pky.gr
     in.feature <- win.gr[i] %over% feature.gr
-    setTxtProgressBar(pb,i)
     if (in.peak==TRUE&in.feature==TRUE){
-      cell.a <- cell.a + 1 
+      cell.a <- cell.a + 1
     } else if (in.peak==FALSE&in.feature==TRUE){
-      cell.b <- cell.b + 1 
+      cell.b <- cell.b + 1
     } else if (in.peak==TRUE&in.feature==FALSE){
       cell.c <- cell.c + 1
     } else if (in.peak==FALSE&in.feature==FALSE){
@@ -73,7 +91,7 @@ window_cont_table <- function(windows.gr,feature.gr,pky.gr){
       stop("Window does not qualify.")
     }
   }
-  close(pb)
+  ##close(pb)
   m <- matrix(c(cell.a,cell.c,cell.b,cell.d),nrow=2,ncol=2)
   rownames(m) <- c("peaky","no.peaky")
   colnames(m) <- c("feature","no.feature")
@@ -94,8 +112,8 @@ build_contingency_table <- function(feature.df,feature.name,peaky.df,peaky.name)
       message("peaky results not available for capture: " %&% capt)
     }
   }
-  saveRDS(object=full.mat,file=out.dir %&% "contingency-table" %&% 
-            "_"%&%peaky.name%&%"_"%&%feature.name%&%".RDS")  
+  saveRDS(object=full.mat,file=out.dir %&% "contingency-table" %&%
+            "_"%&%peaky.name%&%"_"%&%feature.name%&%".RDS")
 }
 build_contingency_table(feature.df,feature.name,capc.pky.df,"capC")
 build_contingency_table(feature.df,feature.name,pchic.pky.df,"pcHiC")
